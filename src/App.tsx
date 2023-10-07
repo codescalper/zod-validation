be20c3515d2b1b0abafca1d15a6362bcd3224ad4
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-07 05:43:26 +0000

    Basic validation from zod

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { z } from 'zod';
 function App() {
 
   const userSchema = z.object({
-    username: z.string(),
-    age: z.number().optional(),
+    username: z.string().min(3),
+    age: z.number().gt(5).optional(),
     isAbove18: z.boolean(),
     birthday: z.date(),
-    test: z.any()
+    test: z.any(),
+    hobbies: z.enum(['programming', 'guitar'])
   }) //Every validation is required in zod you need to use these field
   //* if not then you can use optional method
 
-  const user = { username: "Mayank" };
+  const user = { username: "Hek", age: 6, isAbove18: true, birthday: new Date(), test: 542, hobbies: "guitar" };
 
   const ans = userSchema.parse(user)
   console.log(userSchema.safeParse(user).success)
@@ -23,7 +24,7 @@ function App() {
 
   return (
     <>
-      <p>{ans.username}</p>
+      <p>{ }</p>
     </>
   )
 }
