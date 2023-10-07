2d4c6b6f5734ff904c3f4a3412673e3b64d84ba8
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-07 05:18:56 +0000

    Learned about basic types in zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { TypeOf, z } from 'zod';
+import { z } from 'zod';
 
 function App() {
 
   const userSchema = z.object({
     username: z.string(),
-  })
+    age: z.number().optional(),
+    isAbove18: z.boolean(),
+    birthday: z.date(),
+    test: z.any()
+  }) //Every validation is required in zod you need to use these field
+  //* if not then you can use optional method
 
   const user = { username: "Mayank" };
 
   const ans = userSchema.parse(user)
-  console.log(userSchema.parse(user))
+  console.log(userSchema.safeParse(user).success)
 
   //cool thing is we don't need to create new type for typescript
   // * we can use infer
