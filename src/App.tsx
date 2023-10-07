7b0b04b301852f142b5d57374c4e64a3b299c19d
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-07 05:11:55 +0000

    Learned about zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css'
+import { TypeOf, z } from 'zod';
 
 function App() {
 
+  const userSchema = z.object({
+    username: z.string(),
+  })
+
+  const user = { username: "Mayank" };
+
+  const ans = userSchema.parse(user)
+  console.log(userSchema.parse(user))
+
+  //cool thing is we don't need to create new type for typescript
+  // * we can use infer
+
+  type User = z.infer<typeof userSchema>
 
   return (
     <>
-
+      <p>{ans.username}</p>
     </>
   )
 }
